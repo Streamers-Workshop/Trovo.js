b95e92f08c00d1cff1c08af41196bd2d8e37832f
Bioblaze Payne <bioblazepayne@gmail.com>
2020-10-18 19:55:20 -0700

    This should fix the issue with the Cookie Window, as well as the issue with raids.

--- apollo_example.js
+++ apollo_example.js
@@ -2,31 +2,10 @@ const trovo = require('./lib/client/BrowserClient.js')
 
 const settings = require('../settings.dev.json');
 
-console.log(settings);
-const main = async function () {
-  try {
-    //const trovoApollo = new TrovoApollo()
-    const client = new trovo({ headless: false });
-    await client.login(
+//console.log(settings);
+const client = new trovo({ headless: false });
+    client.login(
       settings.trovo.page,
-      settings.owner.email,
-      settings.owner.password,
+      settings.trovo.email,
+      settings.trovo.password,
     );
-    var data = await client.subs();
-    console.log(data);
-    //const { getLiveInfo } = await trovoApollo.GetLiveInfo('Pirolyx')
-    //console.log(getLiveInfo)
-
-    // SetTitle require login
-     //var data = await trovoApollo.getToken();
-     //console.log(data);
-     //var subs = await trovoApollo.GetSubscribers(0, 10);
-     //console.log(subs);
-    // await trovoApollo.SetTitle('Bonjour à tous')
-
-  } catch (error) {
-    console.error(error)
-  }
-}
-
- main()
